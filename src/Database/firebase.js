abb13b5c45347d8afaaf8834778b6855cc0c243b
Krishna Kumar <kkp785216@gmail.com>
2022-07-28 18:54:04 +0530

    initial commit, making a amazon challenge project

---
+++ src/Database/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABJ05LSut33pHLE08XnYXHWNJ1MHV5jBc",
+    authDomain: "challenge-942ae.firebaseapp.com",
+    projectId: "challenge-942ae",
+    storageBucket: "challenge-942ae.appspot.com",
+    messagingSenderId: "228874922654",
+    appId: "1:228874922654:web:c51a315e75fb092472e98f"
+  };
