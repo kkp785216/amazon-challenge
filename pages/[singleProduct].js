02333e3e795066ce08e578e358a9b0b2961cfaf6
Krishna Kumar <kkp785216@gmail.com>
2022-08-01 23:58:56 +0530

    singleProduct page fixed

--- pages/[singleProduct].js
+++ pages/[singleProduct].js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const SingleProduct = () => {
     const router = useRouter();
     const { singleProduct } = router.query
+
+
   return (
     <div>
         Are you looking for {singleProduct}?
