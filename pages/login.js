aa4bf0bcf1864a4f00dd6283865dcd1123edb730
KRisHnA <kkp785216@gmail.com>
2022-08-15 15:59:52 +0530

    login improved

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
 const Login = () => {
 
+    const [loginForm, setLoginForm] = useState({ email: '', password: '' });
+    const [toggleLogin, setToggleLotin] = useState(true);
+
     useEffect(() => {
         document.getElementById('login-email')?.focus();
     }, []);
+
+    const handleToggleLogin = (e) => {
+        e.preventDefault();
+        setToggleLotin(false);
+    }
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+    }
     return (<>
         <Head>
             <title>Amazon Challenge - Login</title>
@@ -18,9 +30,14 @@ const Login = () => {
                 <Link href='/'><a><img src="/assets/logo_alt.png" className='login__logo' width='103' alt="" /></a></Link>
                 <form className="login__card">
                     <h1 className='login__heading'>Sign-In</h1>
-                    <label htmlFor="login-email">Email or mobile phone number</label>
-                    <input type="text" id="login-email" />
-                    <button type="submit" className='login__button'>Continue</button>
+                    {toggleLogin ? <>
+                        <label htmlFor="login-email">Email or mobile phone number</label>
+                        <input type="text" id="login-email" value={loginForm.email} onChange={(e) => { setLoginForm({ ...loginForm, email: e.target.value }) }} /></> : <>
+                        <label htmlFor="login-password">Password</label>
+                        <input type="password" id="login-password" value={loginForm.password} onChange={(e) => { setLoginForm({ ...loginForm, password: e.target.value }) }} /></>}
+                    {toggleLogin ?
+                        <button type="button" onClick={handleToggleLogin} className='login__button'>Continue</button> :
+                        <button type="submit" onClick={handleLoginSubmit} className='login__button'>Sin in</button>}
                     <p className="login__policy">By continuing, you agree to Amazon&apos;s <span className='login__hyperlink'>Conditions of Use</span> and <span className='login__hyperlink'>Privacy Notice.</span></p>
                 </form>
                 <div className="logon__newUser">
